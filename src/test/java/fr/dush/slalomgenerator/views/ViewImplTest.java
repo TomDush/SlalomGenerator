f2d4195c50d59959bb9d6841ac00563e902cc8f5
Thomas Duchatelle <duchatelle.thomas@gmail.com>
2013-03-28 15:19:08 +0100

    [JUNIT] Ignore junit test which open windows.

--- src/test/java/fr/dush/slalomgenerator/views/ViewImplTest.java
+++ src/test/java/fr/dush/slalomgenerator/views/ViewImplTest.java
@@ -2,11 +2,13 @@ package fr.dush.slalomgenerator.views;
 
 import javax.inject.Inject;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.test.context.ContextConfiguration;
 
 import fr.dush.slalomgenerator.engine.AbstractJunitTest;
 
+@Ignore("Open window...")
 @ContextConfiguration(locations = { "classpath:spring/persistence-mock.xml" })
 public class ViewImplTest extends AbstractJunitTest {
 
